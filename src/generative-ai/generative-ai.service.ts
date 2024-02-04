f293b4b26d9376826916e3f9c2c6c18c7adf1a53
siwat jermwatthana <siwat.j@hiveground.com>
2024-02-04 19:10:03 +0700

    [Feat]: Add genrate ai chat with image

--- src/generative-ai/generative-ai.service.ts
+++ src/generative-ai/generative-ai.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GoogleGenerativeAI, InputContent } from '@google/generative-ai';
 
 @Injectable()
 export class GenerativeAiService {
@@ -19,5 +19,32 @@ export class GenerativeAiService {
       console.log('🚀 ~ PromptService ~ create ~ error:', error);
       throw new HttpException('Failed to generate content', 500);
     }
+  }
+
+  async generateContentWithImage(
+    prompt: string,
+    file: string,
+    mimeType: string,
+  ) {
+    const model = this.genAI.getGenerativeModel({ model: 'gemini-pro-vision' });
+    const imageParts = [this.fileToGenerativePart(file, mimeType)];
+
+    try {
+      const result = await model.generateContent([prompt, ...imageParts]);
+      const response = await result.response;
+      return response.text();
+    } catch (error) {
+      console.log('🚀 ~ PromptService ~ create ~ error:', error);
+      throw new HttpException('Failed to generate content', 500);
+    }
+  }
+
+  fileToGenerativePart(file: string, mimeType: string) {
+    return {
+      inlineData: {
+        data: file,
+        mimeType,
+      },
+    };
   }
 }
